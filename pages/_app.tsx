9c4557d2cd1fc0f766b1e0bad870040a51009a16
Dave Inoc <michael828inoc@gmail.com>
2021-05-26 22:15:21 +0800

    added scroll to TOp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,24 @@ import Layout from "../containers/Layout";
 import store from "../redux/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", () => {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  }, []);
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
