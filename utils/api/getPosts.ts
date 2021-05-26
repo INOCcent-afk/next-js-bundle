238692a7ecff7f93896d1b39830147c4b7cbd729
Dave Inoc <michael828inoc@gmail.com>
2021-05-26 22:05:06 +0800

    add right path

--- utils/api/getPosts.ts
+++ utils/api/getPosts.ts
@@ -1,11 +1,11 @@
 export const getPosts = () => {
-  return fetch(`http://jsonplaceholder.typicode.com/posts`)
+  return fetch(`https://jsonplaceholder.typicode.com/posts`)
     .then((response) => response.json())
     .catch((error) => ({ error }));
 };
 
 export const getSinglePost = (id: string) => {
-  return fetch(`http://jsonplaceholder.typicode.com/posts/${id}`).then(
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
     (response) => response.json().catch((error) => ({ error }))
   );
 };
--- utils/api/getUsers.ts
+++ utils/api/getUsers.ts
@@ -1,11 +1,11 @@
 export const getUsers = () => {
-  return fetch(`http://jsonplaceholder.typicode.com/users`)
+  return fetch(`https://jsonplaceholder.typicode.com/users`)
     .then((response) => response.json())
     .catch((error) => ({ error }));
 };
 
 export const getSingleUser = (id: string) => {
-  return fetch(`http://jsonplaceholder.typicode.com/users/${id}`).then(
+  return fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(
     (response) => response.json().catch((error) => ({ error }))
   );
 };
